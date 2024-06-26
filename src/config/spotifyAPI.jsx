59ac7257b88df9a589946a39e9eed6078957bef4
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-26 12:18:43 +0200

    add spotify api

---
+++ src/config/spotifyAPI.jsx
@@ -0,0 +1,45 @@
+import accessToken from "./apiKeys";
+
+
+const authorization = {
+    headers:{
+        'Authorization': 'Bearer ' + accessToken,
+    }
+}
+
+const setUrl = (type, value) =>{
+
+    let url = '';
+    const urlCore = 'https://api.spotify.com/v1/';
+
+    if(type==='search'){
+        url = `${urlCore}/search?q=${value}&type=artist&market=PL&limit=1`;
+    }
+    else{
+        url = `${urlCore}/${type}/${value}`;
+    }
+
+    return url;
+}
+
+const fetchData = async (type, value) =>{
+
+    const url = setUrl(type, value);
+
+    try{
+        const response = await fetch(url, authorization);
+
+        if(response.ok){
+            const data = await response.json();
+            return data;
+        }
+        else{
+            throw new Error(response.status);
+        }
+    }
+    catch(error){
+        console.error('Fetching data error', error);
+    }
+}
+
+export default fetchData;
