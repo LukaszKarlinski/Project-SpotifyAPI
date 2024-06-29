8619159622838dc7bc8d702fcdd565dfba5d280c
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-29 15:04:42 +0200

    add selected style to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,29 @@
 import { IconHomeFilled } from '@tabler/icons-react';
 import { IconSearch } from '@tabler/icons-react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../style/menu.scss';
 
 const Menu = () =>{
 
+    const location = useLocation();
+    const currentPath = location.pathname;
+    
+
     return(
         <nav>
             <ul>
-                <li><Link to='/'>Strona główna</Link><div className='iconWrap'><IconHomeFilled size={30}/></div></li>
-                <li><Link to='/searchArtist'>Znajdź artyste</Link><div className="iconWrap"><IconSearch size={30}/></div></li>
+                <li className={currentPath === '/'? 'selected': ''}>
+                        <Link to='/'>Strona główna</Link>
+                        <div className='iconWrap'>
+                            <IconHomeFilled size={30}/>
+                        </div>
+                </li>
+                <li className={currentPath === '/searchArtist'? 'selected': ''}>
+                        <Link to='/searchArtist'>Znajdź artyste</Link>
+                        <div className="iconWrap">
+                            <IconSearch size={30}/>
+                        </div>
+                </li>
             </ul>
         </nav>
     )
