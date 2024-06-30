6308b749fb532240ec975fa25b43add262e5c950
Lukasz Karlinski <mysticsk733@gmail.com>
2024-06-30 22:53:18 +0200

    add menu for mobile devices

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,24 +1,34 @@
 import { IconHomeFilled } from '@tabler/icons-react';
 import { IconSearch } from '@tabler/icons-react';
+import { IconMenu2 } from '@tabler/icons-react';
 import { Link, useLocation } from 'react-router-dom';
 import '../style/menu.scss';
+import { useState } from 'react';
 
 const Menu = () =>{
 
     const location = useLocation();
     const currentPath = location.pathname;
+
+    const [showMenu, setShowMenu] = useState(false);
+
+
+    const handleMenuBar = () =>{
+        setShowMenu(!showMenu);
+    }
     
 
     return(
         <nav>
             <ul>
-                <li className={currentPath === '/'? 'selected': ''}>
+                <li className={showMenu? 'mobileBar show' : 'mobileBar hide'} onClick={handleMenuBar}><IconMenu2 size={20}/></li>
+                <li className={`${currentPath === '/'? 'selected menuItem': 'menuItem'} ${showMenu? 'show' : 'hide'}`}>
                         <Link to='/'>Strona główna</Link>
                         <div className='iconWrap'>
                             <IconHomeFilled size={30}/>
                         </div>
                 </li>
-                <li className={currentPath === '/searchArtist'? 'selected': ''}>
+                <li className={`${currentPath === '/searchArtist'? 'selected menuItem': 'menuItem'} ${showMenu? 'show' : 'hide'}`}>
                         <Link to='/searchArtist'>Znajdź artyste</Link>
                         <div className="iconWrap">
                             <IconSearch size={30}/>
